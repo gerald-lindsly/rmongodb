22d4945eef6a36c568d4e06bf5ad8f50b306b7c2
gerald-lindsly <gerald.lindsly@gmail.com>
2011-07-07 13:51:56 -0400

    correct setsockopt being called when error & astyled

--- rmongo/src/api.c
+++ rmongo/src/api.c
@@ -11,14 +11,12 @@
 static R_NativePrimitiveArgType sock_connect_t[] = {INTSXP, STRSXP};
 static R_NativePrimitiveArgType sock_close_t[] = {INTSXP};
 
-static const R_CMethodDef CEntries[] = 
-{
-	CDEF(sock_connect),
-	CDEF(sock_close),
+static const R_CMethodDef CEntries[] = {
+    CDEF(sock_connect),
+    CDEF(sock_close),
     {NULL, NULL, 0}
 };
 
-void attribute_visible R_init_rmongo(DllInfo *dll)
-{
+void attribute_visible R_init_rmongo(DllInfo *dll) {
     R_registerRoutines(dll, CEntries, NULL, NULL, NULL);
 }
